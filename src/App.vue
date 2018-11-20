141ac5586fea93e0012f0c8f89c5a82b53096e73
luoqi <luoqi20131001@163.com>
2018-11-20 16:59:19 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app" class="container">
+    <gantt class="left-container" :tasks="tasks"></gantt>
+  </div>
+</template>
+
+<script>
+import Gantt from "@/view/gantt"
+export default {
+  name: 'app',
+  components: {
+    Gantt
+  },
+  data () {
+    return {
+      tasks: {
+        data: [
+          {id: 1, text: 'Task #1', start_date: '15-04-2017', duration: 3, progress: 0.6},
+          {id: 2, text: 'Task #2', start_date: '20-04-2017', duration: 6, progress: 0.4}
+        ],
+        links: [
+          {id: 1, source: 1, target: 2, type: '0'}
+        ]
+      },
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/view/gantt.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrap" ref="gantt">
+    <h1>common!</h1>
+  </div>
+</template>
+
+<script>
+import 'dhtmlx-gantt'
+
+export default {
+  name: 'gantt',
+  props: {
+    tasks: {
+      type: Object,
+      default () {
+        return {data: [], links: []}
+      }
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    gantt.init(this.$refs.gantt)
+    gantt.parse(this.$props.tasks)
+  }
+}
+</script>
+
+<style>
+  @import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+
+  .wrap {
+    height: 100%;
+  }
+</style>
